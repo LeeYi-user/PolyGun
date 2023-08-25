362d61b7377688dbf14931ecf65fc0d28536e40a
LeeYi-user <khes60122@gmail.com>
2023-08-26 01:24:35 +0800

    Revert "add weapon sway"
    This reverts commit fe53a46d2d064e542d5d9cf9545f8806690094a2.

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -21,10 +21,6 @@ public class Gun : MonoBehaviour
 
     [SerializeField] private Animator animator;
 
-    [Header("Sway Settings")]
-    [SerializeField] private float smooth;
-    [SerializeField] private float multiplier;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -50,19 +46,6 @@ public class Gun : MonoBehaviour
             nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
-
-        // get mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * multiplier + 90;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * multiplier;
-
-        // calculate target rotation
-        Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
-        Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
-
-        Quaternion targetRotation = rotationX * rotationY;
-
-        // rotate 
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
     }
 
     IEnumerator Reload()
