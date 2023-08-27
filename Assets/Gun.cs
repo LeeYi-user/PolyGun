9741327f9bc9132aa83d859f159436a52d2e45aa
LeeYi-user <khes60122@gmail.com>
2023-08-27 23:21:05 +0800

    add bullet trail

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -20,6 +20,10 @@ public class Gun : MonoBehaviour
 
     [SerializeField] private Animator animator;
 
+    [SerializeField] private Transform BulletSpawnPoint;
+    [SerializeField] private TrailRenderer BulletTrail;
+    [SerializeField] private float BulletSpeed = 100;
+
     // Update is called once per frame
     void Update()
     {
@@ -66,8 +70,41 @@ public class Gun : MonoBehaviour
 
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+
+            StartCoroutine(SpawnTrail(trail, hit.point, hit.normal, true));
+        }
+        else
+        {
+            TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+
+            StartCoroutine(SpawnTrail(trail, BulletSpawnPoint.position + fpsCam.transform.forward * range, Vector3.zero, false));
+        }
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer Trail, Vector3 HitPoint, Vector3 HitNormal, bool MadeImpact)
+    {
+        Vector3 startPosition = Trail.transform.position;
+        float distance = Vector3.Distance(Trail.transform.position, HitPoint);
+        float remainingDistance = distance;
+
+        while (remainingDistance > 0)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, HitPoint, 1 - (remainingDistance / distance));
+
+            remainingDistance -= BulletSpeed * Time.deltaTime;
+
+            yield return null;
+        }
+
+        Trail.transform.position = HitPoint;
+
+        if (MadeImpact)
+        {
+            GameObject impactGO = Instantiate(impactEffect, HitPoint, Quaternion.LookRotation(HitNormal));
             Destroy(impactGO, 2f);
         }
+
+        Destroy(Trail.gameObject, Trail.time);
     }
 }
