3ba116fd7749731f9108e0d15bd285fb8cabd221
LeeYi-user <khes60122@gmail.com>
2023-08-27 23:59:59 +0800

    change fake gun

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -13,16 +13,26 @@ public class Gun : MonoBehaviour
     public GameObject impactEffect;
 
     public int maxAmmo = 7;
-    public int currentAmmo = 7;
+    private int currentAmmo;
     public float reloadTime = 1.35f;
-    public bool isReloading = false;
-    public float nextTimeToFire = 0f;
+    private bool isReloading = false;
+    private float nextTimeToFire = 0f;
 
     [SerializeField] private Animator animator;
 
     [SerializeField] private Transform BulletSpawnPoint;
     [SerializeField] private TrailRenderer BulletTrail;
     [SerializeField] private float BulletSpeed = 100;
+
+    public Animator fakeAnimator;
+    public ParticleSystem fakeMuzzleFlash;
+    public Transform fakeBulletSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
 
     // Update is called once per frame
     void Update()
@@ -50,10 +60,12 @@ public class Gun : MonoBehaviour
         isReloading = true;
 
         animator.SetBool("isReloading", true);
+        fakeAnimator.SetBool("isReloading", true);
 
         yield return new WaitForSeconds(reloadTime);
 
         animator.SetBool("isReloading", false);
+        fakeAnimator.SetBool("isReloading", false);
 
         currentAmmo = maxAmmo;
         isReloading = false;
@@ -62,7 +74,9 @@ public class Gun : MonoBehaviour
     void Shoot()
     {
         muzzleFlash.Play();
+        fakeMuzzleFlash.Play();
         animator.SetTrigger("isFiring");
+        fakeAnimator.SetTrigger("isFiring");
 
         currentAmmo--;
 
@@ -71,14 +85,18 @@ public class Gun : MonoBehaviour
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+            TrailRenderer fakeTrail = Instantiate(BulletTrail, fakeBulletSpawnPoint.position, Quaternion.identity);
 
             StartCoroutine(SpawnTrail(trail, hit.point, hit.normal, true));
+            StartCoroutine(SpawnTrail(fakeTrail, hit.point, hit.normal, true));
         }
         else
         {
             TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+            TrailRenderer fakeTrail = Instantiate(BulletTrail, fakeBulletSpawnPoint.position, Quaternion.identity);
 
             StartCoroutine(SpawnTrail(trail, BulletSpawnPoint.position + fpsCam.transform.forward * range, Vector3.zero, false));
+            StartCoroutine(SpawnTrail(fakeTrail, fakeBulletSpawnPoint.position + fpsCam.transform.forward * range, Vector3.zero, false));
         }
     }
 
